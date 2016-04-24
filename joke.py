742fc15f88d210a074cb09e22b453c1e93334d3f
tyuijhgs <Mr.Christopher.Blackman@gmail.com>
2016-04-24 18:23:48 -0400

    woops, pretty much deleted everything =P

---
+++ joke.py
@@ -0,0 +1,21 @@
+from __future__ import with_statement
+from sopel import module
+import requests
+
+@module.commands('joke')
+def joke(bot, trigger):
+	
+	#for getting category (not yet tested)
+	type = trigger.group(2)
+
+        query = '?limitTo=["%s"]' % type
+
+        body = requests.get('http://api.icndb.com/jokes/random' + query)
+
+        data = body.json()
+
+        #check if ther data is the correct data
+        if data is None or data['type'] != u'success':
+            return bot.reply('Error at (joke). This is no Joke! Here\'s what the dumb api said:\n' + data)
+
+        bot.say(data['value']['joke'])
