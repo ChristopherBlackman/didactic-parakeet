10195c3da829f0114347087187e0284e9e2637c4
Guy Hughes <g@gxg.me>
2016-04-24 16:36:59 -0400

    reformat

--- joke.py
+++ joke.py
@@ -4,20 +4,20 @@ import requests
 
 @module.commands('joke')
 def joke(bot, trigger):
-	
-	#for getting category (not yet tested)
-	type_temp = trigger.group(2)
-	#to be replaced with type_temp
-	type = 'nerdy'
 
-	query = '?limitTo=["%s"]' % type
+        #for getting category (not yet tested)
+        type_temp = trigger.group(2)
+        #to be replaced with type_temp
+        type = 'nerdy'
 
-	body = requests.get('http://api.icndb.com/jokes/random' + query)
+        query = '?limitTo=["%s"]' % type
+
+        body = requests.get('http://api.icndb.com/jokes/random' + query)
 
         data = body.json()
-	
+
         #check if ther data is the correct data
         if data is None or data['type'] != u'success':
             return bot.reply('Error at (joke). This is no Joke! Here\'s what the dumb api said:\n' + data)
-	
-	bot.say(data['value']['joke'])
+
+        bot.say(data['value']['joke'])
